a67ce691a16441dc18899c399b9a4988ab85660a
Luke Cloud <luke.cloud@gmail.com>
2026-08-13 19:00:15 -0500

    rolling updates

--- modules/netwatch.nix
+++ modules/netwatch.nix
@@ -2,4 +2,12 @@
 
 {
   environment.systemPackages = [ pkgs.netwatch ];
+
+  security.wrappers.netwatch = {
+    source = "${pkgs.netwatch}/bin/netwatch";
+    owner = "root";
+    group = "users";
+    permissions = "u+rx,g+x";
+    capabilities = "cap_net_raw+ep";
+  };
 }
