6997cab0f467043ab0dd404f34bdb6868947b910
Luke Cloud <luke.cloud@gmail.com>
2026-08-16 17:20:21 -0500

    rolling updates

--- modules/gaming/moonlight.nix
+++ modules/gaming/moonlight.nix
@@ -3,6 +3,6 @@
 {
   # Install the Moonlight client for connecting to Sunshine game-streaming hosts.
   environment.systemPackages = [
-    (pkgs.moonlight-qt.override { ffmpeg = pkgs.ffmpeg_8; }) # Qt desktop client for Sunshine game streams.
+    pkgs.moonlight-qt # Qt desktop client for Sunshine game streams.
   ];
 }
